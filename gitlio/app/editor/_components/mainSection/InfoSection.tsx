ef863d8ee31dc91ffb17f67ff47e7a1482b4f601
junyoung <no2jfamily@tukorea.ac.kr>
2024-03-30 21:26:33 +0900

    feat: 파일 탐색기를 통한 이미지 선택 구현

--- gitlio/app/editor/_components/mainSection/InfoSection.tsx
+++ gitlio/app/editor/_components/mainSection/InfoSection.tsx
@@ -6,14 +6,27 @@ import InfoTagList from './infoComponents/InfoTagList';
 
 const infoData: Data = {
   title: '준영이의 포트폴리오',
-  profileImage: '/gitlio.jpg',
+  profileImage: '/gdsc.jpg',
   infoContent:
     'End-to-End 프로젝트에서 팀 리더를 맡아 React를 중심으로 한 프론트엔드 개발을 주도한 경험이 있습니다',
   tagList: ['#Front Enginner', '#Next.js', '#React.js'],
 };
 
-function InfoSection() {
-  const [infoContent, setIntroContent] = useState<string>(infoData.infoContent);
+export default function InfoSection() {
+  const [infoContent, setInfoContent] = useState<string>(infoData.infoContent);
+  const [infoImage, setInfoImage] = useState<string>(infoData.profileImage);
+
+  const handleInfoContentChange = (e: React.FormEvent<HTMLDivElement>) => {
+    setInfoContent(e.currentTarget.textContent || '');
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = (e.target as HTMLInputElement)?.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      setInfoImage(file.name);
+    }
+  };
 
   return (
     <div className="flex flex-row justify-between bg-white mt-10 rounded-3xl p-10">
@@ -22,13 +35,23 @@ function InfoSection() {
         <div className="w-[244px] h-[160px] px-10 my-10 bg-cover bg-center">
           <img
             className="w-full h-full rounded-full"
-            src="/gdsc.jpg"
+            src={infoImage}
             alt="junyoung"
           />
         </div>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => handleImageChange(e)}
+          style={{ display: 'none' }}
+          id="fileInput"
+        />
+        <label htmlFor="fileInput" className="btn">
+          이미지 수정
+        </label>
       </div>
       <div className="flex flex-col items-center justify-around mr-10">
-        <div className="flex flex-row font-bold text-2xl">
+        <div className="flex flex-row font-bold text-2xl mt-10">
           {infoData.title || ''}
         </div>
         <div className="flex flex-row justify-between">
@@ -40,6 +63,7 @@ function InfoSection() {
           className="w-[450px] h-[200px] border rounded-lg my-10 p-5"
           contentEditable="true"
           suppressContentEditableWarning={true}
+          onInput={(e) => handleInfoContentChange(e)}
         >
           {infoContent}
         </div>
@@ -47,5 +71,3 @@ function InfoSection() {
     </div>
   );
 }
-
-export default InfoSection;

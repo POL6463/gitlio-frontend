5a3dba2dc6066da99a67eeb68d7463e9909c8f91
junyoung <no2jfamily@tukorea.ac.kr>
2024-03-31 16:16:40 +0900

    fix: infoContent 입력 시 문장의 첫번째로 커서위치 이동되는 버그 해결

--- gitlio/app/editor/_components/mainSection/InfoSection.tsx
+++ gitlio/app/editor/_components/mainSection/InfoSection.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Data } from '../../(interface)/InfoData';
 import InfoTagList from './infoComponents/InfoTagList';
 
 const infoData: Data = {
   title: '준영이의 포트폴리오',
-  profileImage: '/gdsc.jpg',
+  profileImage:
+    'https://user-images.githubusercontent.com/77673029/215676052-3c6b8760-c5c6-4ce5-8553-5668a4953f12.png',
   infoContent:
     'End-to-End 프로젝트에서 팀 리더를 맡아 React를 중심으로 한 프론트엔드 개발을 주도한 경험이 있습니다. 프로젝트의 기획부터 배포에 이르는 전반적인 개발 프로세스를 이해하고 있습니다. 협업하는 것에 익숙하며, 팀워크와 지속적인 커뮤니케이션 개선에 중점을 두고 있습니다.',
   tagList: ['#Front Enginner', '#Next.js', '#React.js'],
@@ -15,9 +16,25 @@ const infoData: Data = {
 export default function InfoSection() {
   const [infoContent, setInfoContent] = useState<string>(infoData.infoContent);
   const [infoImage, setInfoImage] = useState<string>(infoData.profileImage);
+  const infoContentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (infoContentRef.current) {
+      const range = document.createRange();
+      const selection = window.getSelection();
+      if (selection) {
+        range.selectNodeContents(infoContentRef.current);
+        range.collapse(false);
+        selection.removeAllRanges();
+        selection.addRange(range);
+      }
+    }
+  }, [infoContent]);
 
   const handleInfoContentChange = (e: React.FormEvent<HTMLDivElement>) => {
-    setInfoContent(e.currentTarget.textContent || '');
+    if (infoContentRef.current) {
+      setInfoContent(infoContentRef.current.textContent || '');
+    }
   };
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {

cca54e2c418d9fee4be6d7f2507f830e8a8898ac
junyoung <no2jfamily@tukorea.ac.kr>
2024-03-31 14:55:24 +0900

    style: InfoSection Css 수정

--- gitlio/app/editor/_components/mainSection/InfoSection.tsx
+++ gitlio/app/editor/_components/mainSection/InfoSection.tsx
@@ -8,7 +8,7 @@ const infoData: Data = {
   title: '준영이의 포트폴리오',
   profileImage: '/gdsc.jpg',
   infoContent:
-    'End-to-End 프로젝트에서 팀 리더를 맡아 React를 중심으로 한 프론트엔드 개발을 주도한 경험이 있습니다',
+    'End-to-End 프로젝트에서 팀 리더를 맡아 React를 중심으로 한 프론트엔드 개발을 주도한 경험이 있습니다. 프로젝트의 기획부터 배포에 이르는 전반적인 개발 프로세스를 이해하고 있습니다. 협업하는 것에 익숙하며, 팀워크와 지속적인 커뮤니케이션 개선에 중점을 두고 있습니다.',
   tagList: ['#Front Enginner', '#Next.js', '#React.js'],
 };
 
@@ -24,9 +24,11 @@ export default function InfoSection() {
     const files = (e.target as HTMLInputElement)?.files;
     if (files && files.length > 0) {
       const file = files[0];
-      setInfoImage(file.name);
+      setInfoImage('/' + file.name);
     }
   };
+
+  console.log(infoImage);
 
   return (
     <div className="flex flex-row justify-between bg-white mt-10 rounded-3xl p-10">
@@ -34,7 +36,7 @@ export default function InfoSection() {
         <h1 className="text-3xl font-medium mx-10 mt-10">#Introduction</h1>
         <div className="w-[244px] h-[160px] px-10 my-10 bg-cover bg-center">
           <img
-            className="w-full h-full rounded-full"
+            className="w-full h-full rounded-full text-center"
             src={infoImage}
             alt="junyoung"
           />
@@ -51,16 +53,16 @@ export default function InfoSection() {
         </label>
       </div>
       <div className="flex flex-col items-center justify-around mr-10">
-        <div className="flex flex-row font-bold text-2xl mt-10">
+        <div className="flex flex-row w-full justify-start font-bold text-2xl mt-10">
           {infoData.title || ''}
         </div>
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row justify-start w-full mt-5">
           {infoData.tagList.map((data, index) => (
             <InfoTagList key={index} data={data} />
           ))}
         </div>
         <div
-          className="w-[450px] h-[200px] border rounded-lg my-10 p-5"
+          className="w-[600px] h-[200px] border rounded-lg my-10 p-5"
           contentEditable="true"
           suppressContentEditableWarning={true}
           onInput={(e) => handleInfoContentChange(e)}

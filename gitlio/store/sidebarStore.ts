ceb5ab94a70fc27f4212ef056905059e85b2dc48
Kevin Kim <kevinkim0617kim@gmail.com>
2024-04-03 23:44:50 +0900

    design: experience section & fixed sidebar

---
+++ gitlio/store/sidebarStore.ts
@@ -0,0 +1,14 @@
+// store/sidebarStore.ts
+import create from 'zustand';
+
+interface SidebarState {
+  selectedSection: string;
+  setSelectedSection: (section: string) => void;
+}
+
+const useSidebarStore = create<SidebarState>((set) => ({
+  selectedSection: 'experience', // 기본값은 "content"
+  setSelectedSection: (section) => set({ selectedSection: section }),
+}));
+
+export default useSidebarStore;

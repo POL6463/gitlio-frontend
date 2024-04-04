4a43fce7b9f122fae19b37731300eadbb8b5cbb0
Kevin Kim <kevinkim0617kim@gmail.com>
2024-04-04 20:59:16 +0900

    design: 전체 레이아웃 수정 & 사이드바 동적 랜더링 기능 추가

--- gitlio/app/editor/layout.tsx
+++ gitlio/app/editor/layout.tsx
@@ -9,26 +9,22 @@ type Props = {
 };
 export default function EditorLayout({ children }: Props) {
   return (
-    <div className="flex flex-col h-fit">
-      {' '}
+    <div className="flex flex-col min-h-screen bg-base-200">
       {/* 전체 레이아웃을 화면 높이에 맞춤 */}
       <TopBar />
-      <div className="flex justify-between w-full bg-base-200 mt-16 h-full">
+      <div className="flex-grow flex overflow-hidden">
         {' '}
         {/* 높이를 전체로 설정 */}
-        <div className="fixed left-0 top-16 bottom-0 overscroll-none">
+        <div className="fixed left-0 top-16 bottom-0 ">
           {' '}
           {/* 좌측 사이드바에 스크롤 적용 */}
           <LeftSidebar />
         </div>
-        <div className="flex-grow flex justify-center items-start overflow-hidden overscroll-none">
-          <div className="mt-5 h-full overflow-hidden w-full max-w-3xl px-4">
-            {' '}
-            {/* `children` 영역을 뷰포트 높이에 맞추고 스크롤 적용 */}
-            {children}
-          </div>
+        <div className="flex flex-col flex-grow mt-24 overflow-auto">
+          {/* 메인 컨텐츠가 위치할 부분; 여기서는 불필요한 스타일 제거 및 조정 */}
+          <div className="px-4 mx-auto mb-16">{children}</div>
         </div>
-        <div className="fixed right-0 top-16 bottom-0 overflow-y-auto overscroll-none">
+        <div className="fixed right-0 top-16 bottom-0">
           {' '}
           {/* 우측 사이드바에 스크롤 적용 */}
           <BaseSideBar />

58f0fe29d105e4311b14a2f20a009332c95bc3e4
ajeong <jenny2360@tukorea.ac.kr>
2024-04-10 20:47:48 +0900

    fix: topbar, sidebar, children 레이아웃 위치 조정

--- gitlio/app/editor/layout.tsx
+++ gitlio/app/editor/layout.tsx
@@ -6,22 +6,27 @@ import BaseSideBar from '@/app/editor/_components/(rightSideBar)/BaseSideBar';
 type Props = {
   children: React.ReactNode;
 };
+
 export default function EditorLayout({ children }: Props) {
   return (
-    <div className="flex flex-col bg-backGray h-screen">
-      {' '}
-      {/* 화면 전체를 채우기 위해 h-screen 추가 */}
+    <div className="flex flex-col min-h-screen bg-base-200">
       <TopBar />
-      <div className="flex flex-1">
-        {' '}
-        {/* 여기에 flex-1을 추가하여 나머지 공간을 채우게 합니다 */}
-        <LeftSidebar />
-        <div className="flex-1 flex justify-center items-start">
-          {' '}
-          {/* children을 중앙에 정렬합니다 */}
-          {children}
+      {/* TopBar 아래에 메인 컨텐츠와 사이드바들을 배치하기 위한 컨테이너 */}
+      <div className="flex flex-grow">
+        <div className="flex w-full">
+          {/* 좌측 사이드바 */}
+          <div className="flex-none mt-16">
+            <LeftSidebar />
+          </div>
+          {/* 메인 컨텐츠가 위치할 부분 */}
+          <div className="flex-1 flex justify-center overflow-auto">
+            <div className="w-full max-w-4xl py-4 mt-16">{children}</div>
+          </div>
+          {/* 우측 사이드바 */}
+          <div className="flex-none mt-16">
+            <BaseSideBar />
+          </div>
         </div>
-        <BaseSideBar />
       </div>
     </div>
   );

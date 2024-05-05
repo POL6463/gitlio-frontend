a2ebd7564350370d71da5a8af67af51895ce53d8
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-05 14:29:23 +0900

    design: TopIconBar css 수정

--- gitlio/app/editor/_components/TopIconBar.tsx
+++ gitlio/app/editor/_components/TopIconBar.tsx
@@ -2,27 +2,29 @@ import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 export default function TopIconBar() {
   return (
-    <div className="fixed left-[350px] top-[160px] transform -translate-y-1/2 z-50 space-y-6">
-      <div className="flex flex-col items-center w-full">
-        <div className="w-1 bg-cyan-500 h-24 rounded-full"></div>
-        <a
-          href="https://github.com"
-          className="text-cyan-500 hover:text-cyan-400"
-        >
-          <FaGithub size={30} />
-        </a>
-        <a
-          href="https://twitter.com"
-          className="text-cyan-500 hover:text-cyan-400"
-        >
-          <FaTwitter size={30} />
-        </a>
-        <a
-          href="https://linkedin.com"
-          className="text-cyan-500 hover:text-cyan-400"
-        >
-          <FaLinkedin size={30} />
-        </a>
+    <div className="fixed left-[350px] top-[67px] z-50 space-y-8">
+      <div className="flex flex-col justify-evenly items-center w-full h-auto">
+        <div className="w-1 bg-black h-[250px]"></div>
+        <div className="flex flex-col justify-between items-center h-auto">
+          <a
+            href="https://github.com"
+            className="text-black hover:text-white transition-colors duration-300"
+          >
+            <FaGithub size={50} />
+          </a>
+          <a
+            href="https://twitter.com"
+            className="text-blue-500 hover:text-blue-400 transition-colors duration-300"
+          >
+            <FaTwitter size={50} />
+          </a>
+          <a
+            href="https://linkedin.com"
+            className="text-cyan-500 hover:text-cyan-400 transition-colors duration-300"
+          >
+            <FaLinkedin size={50} />
+          </a>
+        </div>
       </div>
     </div>
   );

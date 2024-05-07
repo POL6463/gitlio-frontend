065285c20a068b7e245ff14a181dd9be04f45db1
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-07 19:16:16 +0900

    remove: 필요없는 div 제거

--- gitlio/app/editor/_components/TopBlogBar.tsx
+++ gitlio/app/editor/_components/TopBlogBar.tsx
@@ -8,29 +8,27 @@ export default function TopBlogBar() {
     <div className="fixed left-[350px] top-[67px] z-50 space-y-8">
       <div className="flex flex-col justify-evenly items-center w-full h-auto">
         <div className="w-1 bg-black h-[250px]"></div>
-        <div className="flex flex-col justify-center bg-[#555459] items-center h-auto rounded-xl border-2 border-slate-800 p-2 box-border">
-          <div className="flex flex-col justify-center items-center space-y-2">
-            {contactInfo.blogUrls.map((blog) => (
-              <a
-                key={blog.id}
-                href={blog.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {blog.faviconUrl ? (
-                  <div className="w-10 h-10">
-                    <img
-                      src={blog.faviconUrl}
-                      alt="Blog Icon"
-                      className="w-full h-full"
-                    />
-                  </div>
-                ) : (
-                  ''
-                )}
-              </a>
-            ))}
-          </div>
+        <div className="flex flex-col justify-center bg-[#555459] items-center h-auto rounded-xl border-2 border-slate-800 p-2 box-border space-y-2">
+          {contactInfo.blogUrls.map((blog) => (
+            <a
+              key={blog.id}
+              href={blog.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {blog.faviconUrl ? (
+                <div className="w-10 h-10">
+                  <img
+                    src={blog.faviconUrl}
+                    alt="Blog Icon"
+                    className="w-full h-full"
+                  />
+                </div>
+              ) : (
+                ''
+              )}
+            </a>
+          ))}
         </div>
       </div>
     </div>

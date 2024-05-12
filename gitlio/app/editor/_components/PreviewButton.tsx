a847171b4ab515a72aa5d13eb075ef1f1e8c197b
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-12 16:52:23 +0900

    feat: 상태관리를 통한 미리보기 modal 열림/닫힘 구현

--- gitlio/app/editor/_components/PreviewButton.tsx
+++ gitlio/app/editor/_components/PreviewButton.tsx
@@ -1,36 +1,54 @@
+'use client';
+import React, { useState } from 'react';
 // ModalButton.js
 
 import IntroSection from '@/app/editor/_components/mainSection/IntroSection';
 import SkillSection from '@/app/editor/_components/mainSection/SkillSection';
 import ExperienceSection from '@/app/editor/_components/mainSection/ExperienceSection';
+import ProjSection from './mainSection/ProjSection';
+import ContactSection from './mainSection/ContactSection';
 
 const ModalButton = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const openModal = (): void => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = (): void => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
-      <button
-        className="btn text-lg"
-        onClick={() => {
-          return document.getElementById('my_modal_3').showModal();
-        }}
-      >
+      <button className="btn text-lg" onClick={openModal}>
         미리보기
       </button>
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box w-full max-w-full max-h-full h-full bg-base-200">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </button>
-          </form>
-          <h3 className="font-bold text-lg">Preview</h3>
-          <div className="flex flex-col gap-4 items-center">
-            <IntroSection />
-            <SkillSection />
-            <ExperienceSection />
+      {isModalOpen && (
+        <dialog open className="modal" aria-labelledby="modal-title">
+          <div className="modal-box w-full max-w-full max-h-full h-full bg-base-200">
+            <form method="dialog">
+              <button
+                type="button"
+                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                onClick={closeModal}
+              >
+                ✕
+              </button>
+            </form>
+            <h3 id="modal-title" className="font-bold text-lg">
+              Preview
+            </h3>
+            <div className="flex flex-col gap-4 items-center">
+              <IntroSection />
+              <SkillSection />
+              <ExperienceSection />
+              <ProjSection />
+              <ContactSection />
+            </div>
           </div>
-        </div>
-      </dialog>
+        </dialog>
+      )}
     </>
   );
 };

50266e5e4c41235bfff243915a3742f86b4ffa69
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-12 16:34:17 +0900

    design: TopBlogBar 컴포넌트 위치 변경

--- gitlio/app/editor/_components/TopBlogBar.tsx
+++ gitlio/app/editor/_components/TopBlogBar.tsx
@@ -5,7 +5,7 @@ import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa';
 export default function TopBlogBar() {
   const { contactInfo } = ContactSidebarStore();
   return (
-    <div className="fixed left-[350px] top-[67px] z-50 space-y-8">
+    <div className="fixed right-[680px] top-[67px] z-50 space-y-8">
       <div className="flex flex-col justify-evenly items-center w-full h-auto">
         <div className="w-1 bg-black h-[150px] mt-32"></div>
         <div className="flex flex-col justify-center bg-[#555459] items-center h-auto rounded-xl border-2 border-slate-800 p-2 box-border space-y-2">

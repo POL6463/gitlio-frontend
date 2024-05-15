874219ebefc02f9b49f5a9c05459731cb6cee1ac
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-15 16:21:09 +0900

    design: 레이아웃 미리보기 퍼블리싱 및 미리보기 컴포넌트화 진행

--- gitlio/app/editor/_components/(sideBarLayout)/SectionLayout.tsx
+++ gitlio/app/editor/_components/(sideBarLayout)/SectionLayout.tsx
@@ -1,5 +1,6 @@
 import useLayoutStore from '@/store/layoutDesignStore';
 import React from 'react';
+import LayoutPreview from './LayoutPreview';
 
 type SectionProps = {
   section: string;
@@ -8,14 +9,14 @@ type SectionProps = {
 const SectionLayout = ({ section }: SectionProps) => {
   const { intro, skill, experience, contact } = useLayoutStore();
 
-  const handleOption1 = () => {
+  const handleOptionOne = () => {
     if (section === 'introduction') intro.setOption('option1');
     if (section === 'skill') skill.setOption('option1');
     if (section === 'experience') experience.setOption('option1');
     if (section === 'contact') contact.setOption('option1');
   };
 
-  const handleOption2 = () => {
+  const handleOptionTwo = () => {
     if (section === 'introduction') intro.setOption('option2');
     if (section === 'skill') skill.setOption('option2');
     if (section === 'experience') experience.setOption('option2');
@@ -23,16 +24,22 @@ const SectionLayout = ({ section }: SectionProps) => {
   };
 
   return (
-    <div className="flex flex-col items-center w-96">
+    <div className="flex flex-col items-center w-96 h-auto">
       <h2 className="text-center font-bold">{section} Layout Options</h2>
       {['introduction', 'experience', 'skill', 'contact'].includes(section) && (
         <div className="flex justify-evenly w-full space-x-4 mt-4">
-          <button onClick={handleOption1} className="btn btn-primary">
-            Option 1
-          </button>
-          <button onClick={handleOption2} className="btn btn-secondary">
-            Option 2
-          </button>
+          <div className="flex flex-col items-center">
+            <LayoutPreview section={section} option="option1" />
+            <button onClick={handleOptionOne} className="btn mt-4">
+              Option 1
+            </button>
+          </div>
+          <div className="flex flex-col items-center">
+            <LayoutPreview section={section} option="option2" />
+            <button onClick={handleOptionTwo} className="btn mt-4">
+              Option 2
+            </button>
+          </div>
         </div>
       )}
     </div>

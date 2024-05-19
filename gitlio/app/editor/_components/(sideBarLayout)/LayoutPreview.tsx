56492022834777132280b1cd892771073aafcd30
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-19 15:59:12 +0900

    design: option에 따른 ExperienceSection 조건부 랜더링 완료

--- gitlio/app/editor/_components/(sideBarLayout)/LayoutPreview.tsx
+++ gitlio/app/editor/_components/(sideBarLayout)/LayoutPreview.tsx
@@ -30,7 +30,53 @@ const LayoutPreview = ({ section, option }: LayoutPreviewProps) => {
         )}
       </div>
     );
-  } else if (section === 'contact') {
+  } 
+  else if (section === 'experience') {
+    return (
+      <div className="mt-2 w-32 h-32 flex flex-col items-center justify-center border border-gray-400">
+        {option === 'option1' ? (
+          <div className="flex flex-col w-full p-2">
+            <div className="flex items-start justify-between">
+              <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+              <div className="flex flex-col justify-center ml-2">
+                <div className="bg-gray-300 w-16 h-4 mb-2"></div>
+                <div className="bg-gray-300 w-12 h-4 mb-2"></div>
+                <div className="bg-gray-300 w-20 h-4"></div>
+              </div>
+            </div>
+            <div className="flex items-start justify-between mt-4">
+              <div className="flex flex-col justify-center mr-2">
+                <div className="bg-gray-300 w-16 h-4 mb-2"></div>
+                <div className="bg-gray-300 w-12 h-4 mb-2"></div>
+                <div className="bg-gray-300 w-20 h-4"></div>
+              </div>
+              <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+            </div>
+          </div>
+        ) : (
+          <div className="flex flex-col w-full p-2">
+            <div className="flex items-start justify-between">
+              <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+              <div className="flex flex-col justify-center ml-2">
+                <div className="bg-gray-300 w-16 h-4 mb-2"></div>
+                <div className="bg-gray-300 w-12 h-4 mb-2"></div>
+                <div className="bg-gray-300 w-20 h-4"></div>
+              </div>
+            </div>
+            <div className="flex items-start justify-between mt-4">
+              <div className="flex flex-col justify-center items-end mr-2">
+                <div className="bg-gray-300 w-16 h-4 mb-2"></div>
+                <div className="bg-gray-300 w-12 h-4 mb-2"></div>
+                <div className="bg-gray-300 w-20 h-4"></div>
+              </div>
+              <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+  else if (section === 'contact') {
     return (
       <div className="mt-2 w-32 h-32 flex flex-col items-center justify-center border border-gray-400">
         {option === 'option1' ? (

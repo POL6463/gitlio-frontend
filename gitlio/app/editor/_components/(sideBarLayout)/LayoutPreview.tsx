3075b7dc183b2f7ae46993ce95728fc140ebf064
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-19 17:05:39 +0900

    design: Experience Section 레이아웃 선택 미리보기 구현

--- gitlio/app/editor/_components/(sideBarLayout)/LayoutPreview.tsx
+++ gitlio/app/editor/_components/(sideBarLayout)/LayoutPreview.tsx
@@ -30,53 +30,38 @@ const LayoutPreview = ({ section, option }: LayoutPreviewProps) => {
         )}
       </div>
     );
-  } 
-  else if (section === 'experience') {
+  } else if (section === 'experience') {
     return (
-      <div className="mt-2 w-32 h-32 flex flex-col items-center justify-center border border-gray-400">
+      <div className="mt-2 w-40 h-40 flex flex-col items-center justify-center border border-gray-400">
         {option === 'option1' ? (
-          <div className="flex flex-col w-full p-2">
-            <div className="flex items-start justify-between">
-              <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+          <div className="relative flex flex-col justify-center w-full h-full p-2">
+            <div className="absolute left-10 top-2 bottom-2 w-px bg-gray-300"></div>{' '}
+            {/* 왼쪽 선 추가 */}
+            <div className="w-full flex justify-center items-start mt-1">
               <div className="flex flex-col justify-center ml-2">
-                <div className="bg-gray-300 w-16 h-4 mb-2"></div>
-                <div className="bg-gray-300 w-12 h-4 mb-2"></div>
-                <div className="bg-gray-300 w-20 h-4"></div>
+                <div className="bg-gray-300 w-16 h-8 mb-2"></div>
               </div>
             </div>
-            <div className="flex items-start justify-between mt-4">
-              <div className="flex flex-col justify-center mr-2">
-                <div className="bg-gray-300 w-16 h-4 mb-2"></div>
-                <div className="bg-gray-300 w-12 h-4 mb-2"></div>
-                <div className="bg-gray-300 w-20 h-4"></div>
+            <div className="w-full flex justify-center items-start mt-3">
+              <div className="flex flex-col justify-center ml-2">
+                <div className="bg-gray-300 w-16 h-8 mb-2"></div>
               </div>
-              <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
             </div>
           </div>
         ) : (
-          <div className="flex flex-col w-full p-2">
-            <div className="flex items-start justify-between">
-              <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
-              <div className="flex flex-col justify-center ml-2">
-                <div className="bg-gray-300 w-16 h-4 mb-2"></div>
-                <div className="bg-gray-300 w-12 h-4 mb-2"></div>
-                <div className="bg-gray-300 w-20 h-4"></div>
-              </div>
+          <div className="relative flex justify-around w-full h-full p-2">
+            <div className="flex flex-col justify-start mt-7">
+              <div className="bg-gray-300 w-16 h-8 mr-2"></div>
             </div>
-            <div className="flex items-start justify-between mt-4">
-              <div className="flex flex-col justify-center items-end mr-2">
-                <div className="bg-gray-300 w-16 h-4 mb-2"></div>
-                <div className="bg-gray-300 w-12 h-4 mb-2"></div>
-                <div className="bg-gray-300 w-20 h-4"></div>
-              </div>
-              <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+            <div className="absolute left-1/2 top-2 bottom-2 w-px bg-gray-300"></div>{' '}
+            <div className="flex flex-col justify-end mb-7">
+              <div className="bg-gray-300 w-16 h-8 ml-2"></div>
             </div>
           </div>
         )}
       </div>
     );
-  }
-  else if (section === 'contact') {
+  } else if (section === 'contact') {
     return (
       <div className="mt-2 w-32 h-32 flex flex-col items-center justify-center border border-gray-400">
         {option === 'option1' ? (

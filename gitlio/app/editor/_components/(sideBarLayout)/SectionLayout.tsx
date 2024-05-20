fae42d68ed26475450ba1aba9390c22a059769d1
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-05-20 20:24:13 +0900

    feat: option3 상태로 바꿔주는 함수 작성

--- gitlio/app/editor/_components/(sideBarLayout)/SectionLayout.tsx
+++ gitlio/app/editor/_components/(sideBarLayout)/SectionLayout.tsx
@@ -9,25 +9,29 @@ type SectionProps = {
 const SectionLayout = ({ section }: SectionProps) => {
   const { intro, skill, experience, contact } = useLayoutStore();
 
-  const handleOptionOne = () => {
+  const handleOptionOne = (): void => {
     if (section === 'introduction') intro.setOption('option1');
     if (section === 'skill') skill.setOption('option1');
     if (section === 'experience') experience.setOption('option1');
     if (section === 'contact') contact.setOption('option1');
   };
 
-  const handleOptionTwo = () => {
+  const handleOptionTwo = (): void => {
     if (section === 'introduction') intro.setOption('option2');
     if (section === 'skill') skill.setOption('option2');
     if (section === 'experience') experience.setOption('option2');
     if (section === 'contact') contact.setOption('option2');
   };
 
+  const handleOptionThree = (): void => {
+    if (section === 'introduction') intro.setOption('option3');
+  };
+
   return (
     <div className="flex flex-col items-center w-96 h-auto">
       <h2 className="text-center font-bold">{section} Layout Options</h2>
       {['introduction', 'experience', 'skill', 'contact'].includes(section) && (
-        <div className="flex justify-evenly w-full space-x-4 mt-4">
+        <div className="flex flex-wrap justify-evenly w-full mt-4">
           <div className="flex flex-col items-center">
             <LayoutPreview section={section} option="option1" />
             <button
@@ -46,6 +50,16 @@ const SectionLayout = ({ section }: SectionProps) => {
               Option 2
             </button>
           </div>
+          {section === 'introduction' ? (
+            <div className="flex flex-wrap justify-evenly w-full mt-4">
+              <div className="flex flex-col items-center mt-2">
+                <LayoutPreview section={section} option="option2" />
+                <button onClick={handleOptionThree} className="btn mt-4">
+                  Option 3
+                </button>
+              </div>
+            </div>
+          ) : null}
         </div>
       )}
     </div>

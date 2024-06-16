0e8cede0f3c2a23c4959f1a2bac88d50b566477b
ajeong <jenny2360@tukorea.ac.kr>
2024-06-16 22:48:20 +0900

    usenavigate

--- gitlio/app/editor/_components/mainSection/projComponents/ImagePopup.tsx
+++ gitlio/app/editor/_components/mainSection/projComponents/ImagePopup.tsx
@@ -14,38 +14,45 @@ const ImagePopup: React.FC<ImagePopupProps> = ({
   onClose,
   goToNext,
   goToPrevious,
-}) => (
-  <div className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-50">
-    <div className="relative w-auto max-w-3xl mx-auto">
-      <button
-        onClick={onClose}
-        className="absolute top-0 right-0 p-2 text-white text-3xl z-50"
-      >
-        &times;
-      </button>
-      <div className="flex items-center justify-between">
+}) => {
+  // 모달 외부 클릭을 처리하기 위한 함수
+  const handleOutsideClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <div
+      className="fixed inset-0 bg-gray-600 bg-opacity-40 flex justify-center items-center z-50"
+      onClick={handleOutsideClick}
+    >
+      <div className="relative w-auto max-w-3xl mx-auto p-4 bg-white rounded-lg shadow-xl flex items-center">
         <button
           onClick={goToPrevious}
           disabled={images.length <= 1}
-          className="p-3 text-white text-xl bg-gray-800 bg-opacity-50 hover:bg-opacity-75"
+          className="absolute left-0 p-3 text-white text-xl bg-gray-800 bg-opacity-50 hover:bg-opacity-75 rounded-full"
         >
           &lt;
         </button>
         <img
           src={images[currentIndex]}
           alt={`Image ${currentIndex + 1}`}
-          className="block max-h-[80vh] max-w-full mx-auto"
+          className="block h-auto mx-auto"
+          style={{ width: '600px' }} // 이미지의 가로 길이를 600px로 고정
         />
         <button
           onClick={goToNext}
           disabled={images.length <= 1}
-          className="p-3 text-white text-xl bg-gray-800 bg-opacity-50 hover:bg-opacity-75"
+          className="absolute right-0 p-3 text-white text-xl bg-gray-800 bg-opacity-50 hover:bg-opacity-75 rounded-full"
         >
           &gt;
         </button>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ImagePopup;
